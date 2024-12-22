32851da3d0a309c20c9385fe26b84894e90c9f73
Joryh-IO <sujeethvizag@gmail.com>
2024-12-22 18:09:15 +0530

    done

--- Assignment-1/script.js
+++ Assignment-1/script.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
-var root = ReactDOM.createRoot(document.querySelector(".root"));
+// var root = ReactDOM.createRoot(document.querySelector(".root"));
 
-var Greet = () =>{
-    return(
-        <>
-        <h1>Hello world</h1>
-        <p>paragraph</p>
-        </>
-    );
-}
+// var Greet = () =>{
+//     return(
+//         <>
+//         <h1>Hello world</h1>
+//         <p>paragraph</p>
+//         </>
+//     );
+// }
 
-root.render(<Greet></Greet>);
+// root.render(<Greet></Greet>);
