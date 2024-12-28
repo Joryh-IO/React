bc34f2ee144978194661aac014ab845e24e088e6
Joryh-IO <sujeethvizag@gmail.com>
2024-12-28 19:42:53 +0530

    something

--- RestoBook/App.js
+++ RestoBook/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import BodyComponent from "./Components/BodyComponent";
 import Card from "./Components/RestoCard";
@@ -6,25 +6,9 @@ import { states } from "./utils/constants";
 
 var root = ReactDOM.createRoot(document.querySelector(".root"));
 
-// function ren(asd){
-//     return <Card name={asd} text={asd}/>
-// }
 
-// root.render(<>
-//     {states.map(ren)}
-// </>);
-
-var maindata;
-const execute = async () => {
-    const api_data = await fetch
-    ("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.385044&lng=78.486671&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
-    const dataSet = await api_data.json();
-    maindata = dataSet.data.cards;
-    console.log(maindata);
-    
-};
-execute();
-
-
+root.render(<>
+    <BodyComponent/>
+</>);
 
 
--- RestoBook/Components/BodyComponent.jsx
+++ RestoBook/Components/BodyComponent.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
-// const execute = async ()=>{
-//     const api_data = await fetch(
-//         "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.385044&lng=78.486671&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
-// };
-// const dataSet = await api_data.json();
-// console.log(dataSet);
+const [mainData, setMainData] = useState([]);
 
+var BodyComp = () => {
+    var maindata;
+    const execute = async () => {
+        const api_data = await fetch
+        ("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.385044&lng=78.486671&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        const dataSet = await api_data.json();
+        maindata = dataSet.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
+        setMainData(maindata);
+        console.log(maindata);
+        
+    };
+
+    function renderAll(item){
+        return(<Card title={info.name} ratings={info.avgRating} duration={info.sla.slaString} />)
+    }
+};
+
+export default BodyComp;
